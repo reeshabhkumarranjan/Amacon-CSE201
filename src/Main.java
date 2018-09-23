7d9c620faa064791d4ff087f2bfb789534067de1
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2018-09-23 12:02:06 +0530

    Added the main() function in a try-catch block to catch remaining unexpected errors.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ECommerceApp Amacon=new ECommerceApp();
+        try {
+            ECommerceApp Amacon=new ECommerceApp();
+        } catch (Exception e) {
+            IO.println("Invalid input! Exiting the application...");
+            System.exit(1);
+        }
     }
 }
