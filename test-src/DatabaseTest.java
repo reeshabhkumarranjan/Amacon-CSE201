58ea0d1405ac5eee407e01239ec411cb51eaccc3
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2018-09-30 15:48:02 +0530

    Added minimal documentation to the DatabaseTest.java file.

--- test-src/DatabaseTest.java
+++ test-src/DatabaseTest.java
@@ -1,11 +1,8 @@
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
 
 import java.io.*;
 
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DatabaseTest {
 
     private String giveInput(Object o){
@@ -13,71 +10,74 @@ public class DatabaseTest {
         return o.toString()+"\n";
     }
 
-    private InputStream consoleIn;
-    private PrintStream consoleOut;
+    private static InputStream consoleIn; // Saves the console's input stream.
+    private static PrintStream consoleOut; // Saves the console's output stream.
 
-    @Before
-    public void setUp(){
+    @BeforeClass
+    public static void setUp(){
         consoleIn = System.in;
         System.setIn(consoleIn);
         IO.resetScanner(consoleIn);
 
         consoleOut=System.out;
+
+        // To suppress console's output.
+
+        ByteArrayOutputStream out=new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        IO.resetPrinter(new PrintStream(out));
+    }
+
+    @AfterClass
+    public static void tearDown(){
+
+        // To reset input stream to console input.
+
+        System.setIn(consoleIn);
         System.setOut(consoleOut);
-        IO.resetPrinter(consoleOut);
     }
 
     @Test(expected = ProductAlreadyExistsException.class)
     public void insertSameProduct() throws ProductAlreadyExistsException{
+
+        // To re-route console's input stream to custom input from string.
+
         ByteArrayInputStream in=new ByteArrayInputStream((giveInput(1)+giveInput(10)).getBytes());
         System.setIn(in);
         IO.resetScanner(in);
-
-        ByteArrayOutputStream out=new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        IO.resetPrinter(new PrintStream(out));
 
         Database database=new Database();
         database.insertProduct("a>b","c");
         database.insertProduct("a>b","c");
 
-        System.setIn(consoleIn);
-        System.setOut(consoleOut);
+
     }
 
     @Test(expected = InvalidPathException.class)
     public void deleteNonExistentProduct() throws ProductAlreadyExistsException,InvalidPathException{
+
+        // To re-route console's input stream to custom input from string.
+
         ByteArrayInputStream in=new ByteArrayInputStream((giveInput(1)+giveInput(10)).getBytes());
         System.setIn(in);
         IO.resetScanner(in);
-
-        ByteArrayOutputStream out=new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        IO.resetPrinter(new PrintStream(out));
 
         Database database=new Database();
         database.insertProduct("a>b","c");
         database.delete("a>b>d");
-
-        System.setIn(consoleIn);
-        System.setOut(consoleOut);
     }
 
     @Test(expected = ProductNotFoundException.class)
     public void searchNonExistentProduct() throws ProductAlreadyExistsException,ProductNotFoundException{
+
+        // To re-route console's input stream to custom input from string.
+
         ByteArrayInputStream in=new ByteArrayInputStream((giveInput(1)+giveInput(10)).getBytes());
         System.setIn(in);
         IO.resetScanner(in);
 
-        ByteArrayOutputStream out=new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        IO.resetPrinter(new PrintStream(out));
-
         Database database=new Database();
         database.insertProduct("a>b","c");
         database.searchProduct("d",false);
-
-        System.setIn(consoleIn);
-        System.setOut(consoleOut);
     }
 }
